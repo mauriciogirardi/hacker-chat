6de11c6d549c1b64f9b03adf00b2241105a07f31
Mauricio Girardi <maurigirarde@yahoo.com.br>
2021-03-23 17:09:54 -0300

    protocol http

---
+++ client/src/constants.js
@@ -0,0 +1,13 @@
+export const constants = {
+  events: {
+    app: {
+      MESSAGE_SENT: 'message:sent',
+      MESSAGE_RECEIVED: 'message:received',
+      ACTIVITYLOG_UPDATE: 'activityLog:update',
+      STATUS_UPDATED: 'status:updated'
+    },
+
+    socket: {}
+
+  }
+}
